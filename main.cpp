608734ef2da45a6b23514d99d0af8b0135ebebb0
tsuki <226022035@qq.com>
2025-10-27 22:37:57 +0800

    111

--- main.cpp
+++ main.cpp
@@ -2,18 +2,21 @@
 #include <windows.h>
 
 
-int testInput(){
+std::pair<int,int> testInput(){
     int number;
-    std::cout << "请输入：";
+    int num;
+    std::cout << "请输入第一个数字：";
     std::cin >> number;
-    return number;
+    std::cout << "请输入第二个数字：";
+    std::cin >> num;
+    return {number, num};
 }
 
 int main() {
     // 设置控制台输出为 UTF-8
     SetConsoleOutputCP(65001);
 
-    int result = testInput();
-    std::cout << "你输入的是: " << result << std::endl;
+    auto [a, b] = testInput();
+    std::cout << "你输入的是: " << a << "和" << b << std::endl;
     return 0;
 }
