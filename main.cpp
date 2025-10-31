5edd3325d69fec1c01c02d37fb458ce89850a95d
wuricong <15070301734@163.com>
2025-10-31 10:12:10 +0800

    添加跨平台兼容

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-#include <windows.h>
 #include <iomanip>
+#include <utility> // for std::pair
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
-std::pair<double,double> testInput(){
+std::pair<double, double> testInput()
+{
     double number;
     double num;
     std::cout << "请输入第一个数字：";
@@ -13,29 +17,41 @@ std::pair<double,double> testInput(){
     return {number, num};
 }
 
-int main() {
+int main()
+{
     int num;
+
+#ifdef _WIN32
     // 设置控制台输出为 UTF-8
     SetConsoleOutputCP(65001);
+#endif
 
     auto [a, b] = testInput();
     // std::cout << "你输入的是: " << a << "和" << b << std::endl;
 
-      // 检查输入操作是否成功
-    if (std::cin.fail()) {
+    // 检查输入操作是否成功
+    if (std::cin.fail())
+    {
         std::cerr << "Invalid input!" << std::endl;
-    } else {
+    }
+    else
+    {
         // 打印和，保留 6 位小数（根据需要调整）
         std::cout << std::fixed << std::setprecision(6);
         std::cout << "You entered: " << (a + b) << std::endl;
     }
 
-    if(a > b) {
+    if (a > b)
+    {
         std::cout << a << "大于" << b;
-    } else if (a < b) {
+    }
+    else if (a < b)
+    {
         std::cout << a << "小于" << b;
-    } else {
-        std::cout << a << "等于" << b; 
+    }
+    else
+    {
+        std::cout << a << "等于" << b;
     }
     return 0;
 }
